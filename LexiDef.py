29f468636cebe768d90c1cc044c5b8811f350fcc
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-06-21 14:31:50 +0200

    Add files via upload

--- LexiDef.py
+++ LexiDef.py
@@ -1,6 +1,6 @@
 input_file = 'OneFile.txt'
 words_file = 'Lexi.txt'
-definitions_file = 'Def.txt'
+examples_file = 'Examples.txt'
 
 with open(input_file, 'r', encoding='utf-8') as file:
     lines = file.readlines()
@@ -11,12 +11,18 @@ example_lines = []
 for line in lines:
     line = line.strip()
     if line:
-        word, definition, example = line.split('\t')
+        components = line.split('\t')
+        if len(components) == 3:
+            word, definition, example = components
+            example_lines.append(example)
+        elif len(components) == 2:
+            word, definition = components
+        else:
+            continue
         word_lines.append(f'{word}: {definition}')
-        example_lines.append(example)
 
 with open(words_file, 'w', encoding='utf-8') as file:
     file.write('\n'.join(word_lines))
 
-with open(definitions_file, 'w', encoding='utf-8') as file:
+with open(examples_file, 'w', encoding='utf-8') as file:
     file.write('\n'.join(example_lines))
